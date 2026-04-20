e72fb254b1d54228f82de131f43a2f2d04be46b0
kknas <142768324+Ariuka0@users.noreply.github.com>
2026-04-20 16:08:04 +0800

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://etqldullyeckeqkcktbl.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_a-do6BSYH31nlyTEcKOX9A_JlGyYwCe"
 };
